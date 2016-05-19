8c87d8c39d212cd3c3b9b07d961316e64d490767
Jeremy Nicklas <jnicklas@osc.edu>
2016-05-19 16:20:22 -0400

    updated comments

--- lib/ood_app/files_rack_app.rb
+++ lib/ood_app/files_rack_app.rb
@@ -3,9 +3,16 @@ module OodApp
   # path info of the Rack request.
   # @see http://www.rubydoc.info/github/rack/rack/master/Rack/Directory Descripton of `Rack::Directory`
   class FilesRackApp
+    # The routes path used for this rack app
+    # @return [String] the routes path
     attr_accessor :route_path
+
+    # The routes helper used for this route
+    # @return [String] the routes helper
     attr_accessor :route_helper
 
+    # @param route_path [String] the routes path for this rack app
+    # @param route_helper [String] the helper used in the routes
     def initialize(route_path: '/files', route_helper: 'files')
       @route_path   = route_path
       @route_helper = route_helper
