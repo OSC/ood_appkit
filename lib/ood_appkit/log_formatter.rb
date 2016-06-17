e8de11794c63ce812cc7cd758ceb27714275805b
Eric Franz <efranz@osc.edu>
2016-06-17 17:29:58 -0400

    no need for formatter to subclass simpleformatter

--- lib/ood_appkit/log_formatter.rb
+++ lib/ood_appkit/log_formatter.rb
@@ -3,7 +3,7 @@ module OodAppkit
   #
   #     [2016-06-17 15:31:01 -0400 sys/dashboard]  INFO  GET...
   #
-  class LogFormatter < ActiveSupport::Logger::SimpleFormatter
+  class LogFormatter
     def call(severity, timestamp, progname, msg)
       severity_d = severity ? severity[0,5].rjust(5).upcase : "UNKNO"
       timestamp_d = timestamp ? timestamp.localtime : Time.now.localtime
