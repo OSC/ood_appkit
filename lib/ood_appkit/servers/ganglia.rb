b1c10f4fc70b3f88f1adc533af4510e84f1a9e14
Jeremy Nicklas <jnicklas@osc.edu>
2016-07-15 16:58:58 -0400

    renamed variable for clearer context

--- lib/ood_appkit/servers/ganglia.rb
+++ lib/ood_appkit/servers/ganglia.rb
@@ -21,13 +21,17 @@ module OodAppkit
 
       # The required query values of the URI
       # @example Required cluster query value
-      #   "my_ganglia.query"
+      #   "my_ganglia.req_query"
       #   #=> {c: "MyCluster"}
       # @return [Hash] required query values of uri
-      attr_reader :query
+      attr_reader :req_query
 
       # Optional query values of the URI, these query values are
       # only defined if specified
+      # NB: All optional values will be evaluated for string interpolations
+      # @example Optional host query value
+      #   "my_ganglia.opt_query"
+      #   #=> {h: "%{h}.ten.osc.edu"}
       # @return [Hash] optional query values of uri
       attr_reader :opt_query
 
@@ -36,48 +40,44 @@ module OodAppkit
       attr_reader :version
 
       # @param scheme [String] the scheme used for URI
-      # @param segments [Array<String>] the segments used to
-      #   construct path of URI
-      # @param query [Hash] hash of query values used for
-      #   URI
-      # @param opt_query [Hash] hash of optional query
-      #   values if they exist
+      # @param segments [Array<String>] the segments used to construct path of URI
+      # @param req_query [Hash] hash of required query values used for URI
+      # @param opt_query [Hash] hash of optional query values if they exist
       # @param version [String] version of server software
-      def initialize(scheme:, segments: [], query: {}, opt_query: {}, version:, **kwargs)
+      def initialize(scheme:, segments: [], req_query: {}, opt_query: {}, version:, **kwargs)
         super(kwargs)
 
         # uri
         @scheme = scheme
         @segments = segments
-        @query = query
+        @req_query = req_query
         @opt_query = opt_query
 
         # version
         @version = version
       end
 
-      # The full hash of query values used to construct URI
-      # @param other_query [Hash] user specified query hash
-      # @return [Hash] full hash of query values
-      def query_values(other_query)
-        query.merge(
-          other_query.each_with_object({}) do |(k, v), h|
-            h[k] = opt_query.has_key?(k) ? (opt_query[k] % query.merge(other_query)) : v
-          end
-        )
-      end
-
       # The URI used to access information about given cluster
-      # @param query_values [Hash] user specified query hash
+      # @param query [Hash] user specified query hash
       # @return [Addressable] the uri for ganglia server
-      def uri(query_values: {})
+      def uri(query: {})
         Addressable::Template.new(TEMPLATE).expand({
           scheme: scheme,
           host: host,
           segments: segments,
-          query: query_values(query_values)
+          query: query_hash(query)
         })
       end
+
+      private
+        # The full hash of query values used to construct URI
+        def query_hash(query)
+          req_query.merge(
+            query.each_with_object({}) do |(k, v), h|
+              h[k] = opt_query.has_key?(k) ? (opt_query[k] % req_query.merge(query)) : v
+            end
+          )
+        end
     end
   end
 end
