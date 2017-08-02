a05117d271f717d14f532c73940e5fde823553c1
Eric Franz <efranz@osc.edu>
2017-08-02 19:25:56 -0400

    fix test

--- test/ood_appkit_test.rb
+++ test/ood_appkit_test.rb
@@ -8,7 +8,7 @@ class OodAppkitTest < ActiveSupport::TestCase
   # FIXME: move these tests elsewhere
   #
   test "shell urls" do
-    s = OodAppkit::ShellUrl.new(base_url: "/sh")
+    s = OodAppkit::Urls::Shell.new(base_url: "/sh")
 
     assert_equal "/sh/ssh/default", s.url.to_s
     assert_equal "/sh/ssh/oakley", s.url(host: "oakley").to_s
@@ -19,7 +19,7 @@ class OodAppkitTest < ActiveSupport::TestCase
   end
 
   test "files urls" do
-    f = OodAppkit::FilesUrl.new(base_url: "/f")
+    f = OodAppkit::Urls::Files.new(base_url: "/f")
 
     assert_equal "/f/fs/nfs/17/efranz/ood_dev", f.url(path: "/nfs/17/efranz/ood_dev").to_s
     assert_equal "/f/fs/nfs/17/efranz/ood_dev", f.url(path: Pathname.new("/nfs/17/efranz/ood_dev")).to_s
