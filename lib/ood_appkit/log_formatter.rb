25b5bb58c10c34f2e2442659048073f7cb1514d8
Jeff Ohrstrom <johrstrom@osc.edu>
2025-06-16 11:03:58 -0400

    update to work with rails 7.1

--- lib/ood_appkit/log_formatter.rb
+++ lib/ood_appkit/log_formatter.rb
@@ -1,9 +1,14 @@
+require 'active_support/tagged_logging'
+
 module OodAppkit
   # format log messages with timestamp severity and app token e.g.:
   #
   #     [2016-06-17 15:31:01 -0400 sys/dashboard]  INFO  GET...
   #
   class LogFormatter
+
+    include ActiveSupport::TaggedLogging::Formatter
+
     def call(severity, timestamp, progname, msg)
       severity_d = severity ? severity[0,5].rjust(5).upcase : "UNKNO"
       timestamp_d = timestamp ? timestamp.localtime : Time.now.localtime
